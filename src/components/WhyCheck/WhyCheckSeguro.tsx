73fcfd7d8386a76887e112b87c08bf1331aa1c98
adryeldeev <adryeldev@hotmail.com>
2025-06-26 18:26:41 -0300

    atualizando whycheckseguro

--- src/components/WhyCheck/WhyCheckSeguro.tsx
+++ src/components/WhyCheck/WhyCheckSeguro.tsx
@@ -3,17 +3,17 @@ import { FaBalanceScale, FaHeart, FaGlobeAmericas } from 'react-icons/fa'
 
 const WhyCheckSeguro = () => {
   return (
-    <div className="flex flex-col gap-6 max-w-md w-full m-5">
+    <div className="flex flex-col gap-6 max-w-md w-full">
       {/* Título: maior no desktop, menor no mobile */}
-      <h2 className="text-xl sm:text-3xl md:text-3xl lg:text-3xl font-bold text-black text-center">
+      <h2 className="text-xl sm:text-2xl md:text-3xl lg:text-3xl font-bold text-black text-center">
   Por que confiar na <span className="text-[#FFD700]">4S Seguro</span>?
 </h2>
 
       {/* Itens: texto menor no mobile */}
       <div className="flex items-start gap-3">
-        <FaBalanceScale className="text-[#FFD700] text-xl sm:text-2xl mt-1" />
+        <FaBalanceScale className="text-[#FFD700] text-xl" />
         <p className="text-sm sm:text-base md:text-lg">
-          Experiência e credibilidade em benefícios e seguros pessoais.
+          Experiência e credibilidade em benefícios  e seguros pessoais.
         </p>
       </div>
 
